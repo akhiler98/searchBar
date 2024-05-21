d09901b31bbd6b11b1820c86b86acc8d85e4b902
akhiler <akhiler19@gmail.com>
2024-05-21 22:50:14 +0200

    changes done

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/searchBar/',
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js','resources/js/Search.jsx'],
@@ -11,8 +11,5 @@ export default defineConfig({
         }),
         react()
     ],
-    build: {
-        outDir: 'docs',
-        },
     
 });
